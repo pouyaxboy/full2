0e19196fe6eee4d8e8e44c6cfa2fbe07fdfa247c
pouyaxboy <offboy098@gmail.com>
2016-03-02 08:45:10 -0800

    Create mirror.lua

---
+++ plugins/mirror.lua
@@ -0,0 +1,39 @@
+--------------------------------------------------
+--      ____  ____ _____                        --
+--     |    \|  _ )_   _|___ ____   __  __      --
+--     | |_  )  _ \ | |/ ·__|  _ \_|  \/  |     --
+--     |____/|____/ |_|\____/\_____|_/\/\_|     --
+--                                              --
+--------------------------------------------------
+--                                              --
+--       Developers: @Josepdal & @MaSkAoS       --
+--     Support: @Skneos,  @iicc1 & @serx666     --
+--                                              --
+--------------------------------------------------
+
+do
+
+function run(msg, matches)
+  return [[
+About From Mirror TG
+
+created By: @pouya_x_Boy
+
+Special thanks to:
+DBTeam
+and
+Yagop
+
+Mirror TG is You'r Group or supergroup manager
+channel: @MirrorTGch
+http://s7.picofile.com/file/8241529292/photo_2016_02_28_10_00_42_Copy.jpg
+end
+
+return {
+  patterns = {
+    "^#Mirror$"
+  }, 
+  run = run 
+}
+
+end
