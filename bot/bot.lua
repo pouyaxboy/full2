bbdcb6a21bbc7ca4e553f24f8e80f54b410ff55a
pouyaxboy <offboy098@gmail.com>
2016-03-03 06:20:30 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -245,7 +245,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {83150569,133478263},
     admin_users = {},
     disabled_channels = {}
   }
